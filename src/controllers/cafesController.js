d714c12e4876d5bed4d63c24fb13220dc3c6d908
yrkeviin <kevineziquiellima160@gmail.com>
2025-02-11 11:39:37 -0300

    Feat: Create listar, add and buscar methos in controller

---
+++ src/controllers/cafesController.js
@@ -0,0 +1,20 @@
+const listarCafes = (req, res) => {
+    res.json(listaCafes.listarCafes());
+}
+
+const addCafe = (req, res) => {
+    const { cliente, pedido, valor } = req.body;
+    if (!cliente || !pedido || !valor) {
+        return res.status(400).json({ erro: "Cliente, pedido e valor são obrigatórios." });
+    }
+    const novoCafe = listaCafes.addCafe(cliente, pedido, valor);
+    res.status(201).json(novoCafe);
+}
+
+const buscarCafe = (req, res) => {
+    const cafe = listaCafes.buscarCafe(parseInt(req.params.id));
+    if(!cafe) {
+        return res.status(404).json({ erro: "Café não encontrado." });
+    }
+    res.json(cafe);
+}
